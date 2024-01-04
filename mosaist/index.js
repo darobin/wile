132e29f2791f9f81fac87ce79322e163608aa586
Robin Berjon <robin.berjon@nytimes.com>
2024-01-04 14:21:58 -0500

    'rpc works'

--- mosaist/index.js
+++ mosaist/index.js
@@ -4,26 +4,25 @@ import { cwd } from 'node:process';
 import { readFile, readdir } from 'node:fs/promises';
 import mime from 'mime-types';
 import { CID } from 'multiformats/cid';
-import { createHelia } from 'helia';
-import { dagCbor } from '@helia/dag-cbor';
-import { raw } from './helia-raw.js';
+import { create } from "kubo-rpc-client";
+
+const ipfs = create();
 
 export default class Mosaist {
   async watchDirectory (maybeRelPath) {
-    const s = new MosaistTileServer();
-    await s.start();
+    const m = new MosaistManifestManager();
     if (!isAbsolute(maybeRelPath)) maybeRelPath = join(cwd(), maybeRelPath);
     const files = (await readdir(maybeRelPath, { recursive: true }))
       .map((f) => ({ relPath: f, fullPath: join(maybeRelPath, f) }))
     ;
     const manEntry = files.find(({ relPath }) => relPath === 'manifest.json');
     if (!manEntry) throw new Error(`No manifest.json at root of tile directory "${maybeRelPath}"`);
-    await s.setManifestFromPath(manEntry.fullPath);
+    await m.setManifestFromPath(manEntry.fullPath);
     const otherFiles = files.filter(({ relPath }) => relPath !== 'manifest.json');
     await Promise.all(
-      otherFiles.map(({ relPath, fullPath }) => s.addFileFromPath(relPath, fullPath))
+      otherFiles.map(({ relPath, fullPath }) => m.addFileFromPath(relPath, fullPath))
     );
-    const rootAddr = await s.serveLatest();
+    const rootAddr = await m.addLatest();
     console.warn(`serving web+tile://${rootAddr}/`);
     // XXX do the watch part too
     // - watch
@@ -33,33 +32,16 @@ export default class Mosaist {
     //  - on delete
     //    - manifest: error
     //    - other: remove, update map + manifest, serve
-    return s;
   }
   async fetchManifest (url) {
-    const helia = await createHelia();
     const cid = new URL(url).hostname;
-    const cbor = dagCbor(helia);
-    const manifest = await cbor.get(CID.parse(cid));
-    await helia.stop();
-    return manifest;
+    return await getDAG(CID.parse(cid));
   }
 }
 
-class MosaistTileServer {
+class MosaistManifestManager {
   constructor () {
-    this.helia = null;
-    this.dagCbor = null;
     this.manifest = { name: 'Untitled', description: '' };
-  }
-  async start () {
-    this.helia = await createHelia({ start: true });
-    this.dagCbor = dagCbor(this.helia);
-    this.raw = raw(this.helia);
-    console.warn(`Mosaist server started…`);
-  }
-  async stop () {
-    if (!this.helia) return;
-    this.helia.stop();
   }
   setManifest (manifest) {
     this.manifest = manifest;
@@ -72,14 +54,33 @@ class MosaistTileServer {
   }
   async addFileFromPath (relPath, fullPath) {
     const mediaType = mime.lookup(fullPath);
-    const cid = await this.raw.add(await readFile(fullPath));
+    const cid = await addRaw(await readFile(fullPath));
     const src = cid.toString();
     if (!this.manifest.resources) this.manifest.resources = {};
     this.manifest.resources[relPath] = { src, mediaType };
     if (relPath === 'index.html') this.manifest.resources['/'] = { src, mediaType };
   }
-  async serveLatest () {
-    const rootAddr = await this.dagCbor.add(this.manifest);
+  async addLatest () {
+    const rootAddr = await addDAG(this.manifest);
     return rootAddr;
   }
 }
+
+async function addDAG (dag) {
+  return await ipfs.dag.put(dag);
+}
+
+async function getDAG (cid) {
+  const { value } = await ipfs.dag.get(cid);
+  return value;
+}
+
+// this probably doesn't chunk for now we don't care
+async function addRaw (bytes) {
+  const cid = await ipfs.block.put(bytes, { version: 1, format: 'raw', pin: true });
+  return cid;
+}
+
+// async function getRaw (cid) {
+//   return await ipfs.block.get(cid);
+// }
