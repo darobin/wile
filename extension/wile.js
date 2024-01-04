750ac1dcb87e16fc6db78b6e9ff718de265f7d6e
Robin Berjon <robin.berjon@nytimes.com>
2024-01-04 15:23:34 -0500

    'split out helia setup and make it use trustless'

--- extension/wile.js
+++ extension/wile.js
@@ -1,24 +1,15 @@
 
-import { createHelia } from 'helia';
-import { dagCbor } from '@helia/dag-cbor';
 import { CID } from 'multiformats/cid';
-import { IDBDatastore } from 'datastore-idb';
-import { IDBBlockstore } from 'blockstore-idb';
-
-let helia;
+import { dagCbor } from '@helia/dag-cbor';
+import { createHelia } from './helia.js';
 
 (async () => {
   // Setup
-  const datastore = new IDBDatastore('helia/datastore');
-  await datastore.open();
-  const blockstore = new IDBBlockstore('helia/blockstore');
-  await blockstore.open();
   const url = new URL(decodeURIComponent(document.location.hash.replace(/^#!url=/, '')));
   console.warn(`Loading ${url.href}…`);
   const cid = CID.parse(url.href.replace(/^web\+tile:\/\//, '').replace(/\/.*/, ''));
   console.warn(`CID=${cid.toString()}`);
-  const node = await createHelia({ datastore, blockstore, libp2p: { connectionManager: { minConnections: 0 }}});
-  helia = dagCbor(node);
+  const helia = dagCbor(await createHelia());
   console.warn(`Helia created`);
   const manifest = await helia.get(cid);
   console.warn(`Manifest loaded`, manifest);
